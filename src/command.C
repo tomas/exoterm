93f869eaadf3726c50f86dff7e15f94b94d00d9d
Tomás Pollak <tomaspollak@gmail.com>
2020-01-15 21:39:50 -0300

    URL parsing: Handle apostrophes leading ssh:// protocols

--- src/command.C
+++ src/command.C
@@ -1010,8 +1010,10 @@ int parse_links(line_t * l, int in_link) {
       l->r[i-1] |= RS_Uline; // s
       l->r[i-2] |= RS_Uline; // p
       l->r[i-3] |= RS_Uline; // p
-      if (l->t[i-4] != ' ') l->r[i-4] |= RS_Uline; // t, might be blank if protocol is shorter (ssh://)
-      if (l->t[i-5] == 'h') l->r[i-5] |= RS_Uline; // h or something else, if this was simply http:
+      if (l->t[i-4] != ' ' && l->t[i-4] != '"' && l->t[i-4] != '\'') {
+        l->r[i-4] |= RS_Uline; // t, might be blank if protocol is shorter (ssh://)
+        if (l->t[i-5] == 'h') l->r[i-5] |= RS_Uline; // h or something else, if this was simply http:
+      }
     }
   }
 
