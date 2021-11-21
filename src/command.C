e278bcd904bb9ff4e7e21b29f9e4d883a170c94b
Marc Lehmann <schmorp@schmorp.de>
2021-11-21 23:34:23 +0000

    Added OSC 776 sequence to return character cell info

--- src/command.C
+++ src/command.C
@@ -27,7 +27,7 @@
  * Copyright (c) 2001      Marius Gedminas
  *				- Ctrl/Mod4+Tab works like Meta+Tab (options)
  * Copyright (c) 2003      Rob McMullen <robm@flipturn.org>
- * Copyright (c) 2003-2014 Marc Lehmann <schmorp@schmorp.de>
+ * Copyright (c) 2003-2021 Marc Lehmann <schmorp@schmorp.de>
  * Copyright (c) 2007,2015 Emanuele Giaquinta <e.giaquinta@glauco.it>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -5922,6 +5922,13 @@ rxvt_term::process_xterm_seq (int op, char *str, string_term &st)
                      op,
                      rs[Rs_name], VERSION[0], VERSION[2],
                      st.v);
+        break;
+
+      case URxvt_cellinfo:
+        if (query)
+          tt_printf ("\33]%d;%d;%d;%d%c", saveop,
+                     fwidth, fheight, fbase,
+                     resp);
         break;
 
 #if !ENABLE_MINIMAL
